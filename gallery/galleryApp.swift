ab30bdaf85b8044f6d78b2c656a571252772edaa
Jean <gofltmxj@naver.com>
2025-04-03 14:00:49 +0900

    Initial Commit

---
+++ gallery/galleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  galleryApp.swift
+//  gallery
+//
+//  Created by Heawon Seo on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct galleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
